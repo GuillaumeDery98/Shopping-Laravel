31f8daa35d8349db0bd52d083e01bfd6e9b6be3e
Guillaume Dery <guillaume@synexis.be>
2020-06-12 16:24:08 +0200

    create models & migration country

--- database/migrations/2020_06_12_142340_create_countries_table.php
+++ database/migrations/2020_06_12_142340_create_countries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateModelsCountriesTable extends Migration
+class CreateCountriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateModelsCountriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('models_countries', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
-            $table->decimal('tax');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ class CreateModelsCountriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('models_countries');
+        Schema::dropIfExists('countries');
     }
 }
